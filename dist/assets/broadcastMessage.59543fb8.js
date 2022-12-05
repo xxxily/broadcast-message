166ce19f124cc8cea9079c382a1ce0f113e699ad
xxxily <974278171@qq.com>
2022-12-05 11:59:41 +0800

    feat: 发布dist

--- dist/assets/broadcastMessage.59543fb8.js
+++ dist/assets/broadcastMessage.59543fb8.js
@@ -1,13 +1,1 @@
-function __vite_legacy_guard() {
-  import("data:text/javascript,");
-}
-;
-import { B as BroadcastMessage } from "./index.5f0dc2ea.js";
-new BroadcastMessage({
-  inTrustedDomainPages: true,
-  allowLocalBroadcast: true,
-  emitOriginalMessage: true
-});
-export {
-  __vite_legacy_guard
-};
+import{B as a}from"./index.5f0dc2ea.js";function e(){import("data:text/javascript,")}new a({inTrustedDomainPages:!0,allowLocalBroadcast:!0,emitOriginalMessage:!0});export{e as __vite_legacy_guard};
--- dist/assets/demo.0f9fc773.js
+++ dist/assets/demo.0f9fc773.js
@@ -1,37 +1,10 @@
-function __vite_legacy_guard() {
-  import("data:text/javascript,");
-}
-;
-import { B as BroadcastMessage } from "./index.5f0dc2ea.js";
-const style = "";
-const javascriptLogo = "/assets/javascript.8dac5379.svg";
-const broadcastMessage = new BroadcastMessage({
-  channelId: "test001",
-  allowLocalBroadcast: true,
-  emitOriginalMessage: false,
-  debug: false
-});
-broadcastMessage.addEventListener("message", function(event) {
-  console.log(`[BroadcastMessage-Event][onMessage]`, event.data);
-});
-function setupCounter(element) {
-  let counter = 0;
-  const setCounter = (count) => {
-    counter = count;
-    element.innerHTML = `\u53D1\u9001\u6D88\u606F [ count:${counter} ]`;
-    broadcastMessage.postMessage(`${Date.now()} - ${counter}`);
-  };
-  element.addEventListener("click", () => setCounter(counter + 1));
-  setCounter(0);
-}
-window.BroadcastMessage = BroadcastMessage;
-document.querySelector("#app").innerHTML = `
+import{B as s}from"./index.5f0dc2ea.js";function l(){import("data:text/javascript,")}const n=""+new URL("javascript.8dac5379.svg",import.meta.url).href,o=new s({channelId:"test001",allowLocalBroadcast:!0,emitOriginalMessage:!1,debug:!1});o.addEventListener("message",function(e){console.log("[BroadcastMessage-Event][onMessage]",e.data)});function c(e){let t=0;const a=r=>{t=r,e.innerHTML=`\u53D1\u9001\u6D88\u606F [ count:${t} ]`,o.postMessage(`${Date.now()} - ${t}`)};e.addEventListener("click",()=>a(t+1)),a(0)}window.BroadcastMessage=s;document.querySelector("#app").innerHTML=`
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/assets/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${n}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello BroadcastMessage</h1>
     <div class="card">
@@ -44,8 +17,4 @@ document.querySelector("#app").innerHTML = `
     \u57FA\u4E8EpostMessage+BroadcastChannel+localStorage+\u4E92\u4FE1\u57DF\u540D\u7684\u524D\u7AEF\u9875\u9762\u6570\u636E\u901A\u4FE1\u89E3\u51B3\u65B9\u6848
     </p>
   </div>
-`;
-setupCounter(document.querySelector("#counter"));
-export {
-  __vite_legacy_guard
-};
+`;c(document.querySelector("#counter"));export{l as __vite_legacy_guard};
